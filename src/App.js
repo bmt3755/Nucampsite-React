a12a88f6059721a1399c2483d1fa79d6ff76b82e
Manjeera <manjeera.thogarcheti@gmail.com>
2021-07-27 15:37:18 -0400

    functional components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Main from './components/DirectoryComponent';
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+ 
+  render() {
+      return (
+          <div className="App">
+              <Main />
+          </div>
+      );
+  }
+}
+
+
+export default App;
---
+++ src/components/DirectoryComponent.js
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { PopperPlacements } from 'reactstrap/lib/utils';
+
+function RenderDirectoryItem({campsite, onClick}) {
+    return (
+        <Card onClick={() => onClick(campsite.id)}>
+            <CardImg width="100%" src = {campsite.image} alt = {campsite.name } />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle> 
+            </CardImgOverlay>
+        </Card>
+    );
+}
+function Directory(props) {    
+        //mapping the data to the html
+        let directory;
+        if(props.campsites){
+            directory = props.campsites.map(campsite => {
+                return(
+                    <div key = {campsite.id} className = "col-md-5 m-1">
+                        <RenderDirectoryItem campsite = {campsite} onClick = {props.onClick} />
+                    </div>
+                    );
+                });
+                
+        }
+  
+        
+            
+        //rendering the mapped data 
+        return(
+            <div className = "container">
+                <div className = "row">
+                    {directory}
+                </div>
+                
+            </div>
+        );
+}
+
+
+
+export default Directory;
