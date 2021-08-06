babce7df9192807e2703363c3fd15d7350c14aa3
Manjeera <manjeera.thogarcheti@gmail.com>
2021-08-06 19:04:53 -0400

    React Redux Form Validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem,
         Button, Label, Col, Row } from 'reactstrap';
-import { Control, LocalForm } from 'react-redux-form';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { render } from '@testing-library/react';
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+const isNumber = val => !isNaN(+val);
+const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends Component {
 
