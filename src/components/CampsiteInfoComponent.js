7d192fb2b74e9f6a64000bf8d619a616cacc7661
Manjeera <manjeera.thogarcheti@gmail.com>
2021-08-15 14:07:03 -0400

    Fetch from Server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -6,6 +6,7 @@ import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import Label from "reactstrap/lib/Label";
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 class CampsiteInfo extends Component {
 
@@ -18,7 +19,7 @@ class CampsiteInfo extends Component {
         return(
             <div className = "col-md-5 m-1">
                 <Card>
-                        <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                         <CardBody>
                             <CardText>{campsite.description}</CardText>
                         </CardBody>
